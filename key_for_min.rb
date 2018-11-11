7802a3f25efb11f7c0c6d583b43fd4824bc8bb54
Albina Zakirova <zak1804@gmail.com>
2018-11-11 19:06:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+ name_hash.min_by{ |name, num| num }
+ key = mane_hash.keys[0]
 
+ 
 end
