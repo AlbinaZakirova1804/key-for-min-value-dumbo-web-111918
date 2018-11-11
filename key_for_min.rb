ecb308b66276235305de8da49127d78c08ad0311
Albina Zakirova <zak1804@gmail.com>
2018-11-11 19:09:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,16 +3,13 @@
 
 def key_for_min_value(name_hash)
   
- #name_hash.min_by{ |name, num| num }
-# key = mane_hash.keys[0]
-min = nil
- min_name = nil
- name_hash.each do |name, num|
+   min = nil
+   min_name = nil
+   name_hash.each do |name, num|
     if min == nil || min > num
       min = num
       min_name = name
     end
-  end
+   end
   min_name
- 
 end
